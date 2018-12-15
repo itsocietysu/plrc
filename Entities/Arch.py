1e492bffd07bb3328c0943c521b43a73710489d7
chironovaa <chironovaa@mail.ru>
2018-12-15 15:23:35 +0300

    Arch placement as array

--- Entities/Arch.py
+++ Entities/Arch.py
@@ -6,14 +6,15 @@ class Arch:
     _type = 'arch'
 
     def __init__(self, _placement=None):
-        self.placement = _placement
+        self.placement = [_placement]
 
     def to_dict(self):
+        placement = [_.to_dict() for _ in self.placement]
         return OrderedDict([('type', Arch._type),
-                            ('placement', self.placement.to_dict())])
+                            ('placement', placement)])
 
     def from_dict(self, obj):
         if 'type' in obj and obj['type'] == Arch._type and 'placement' in obj:
-            self.placement = Line().from_dict(obj['placement'])
+            self.placement = [Line().from_dict(_) for _ in obj['placement']]
 
         return self
