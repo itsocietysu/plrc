460d6c2b3fb8f6011b7c25bb6e1632b414241d8d
chironovaa <chironovaa@mail.ru>
2019-01-31 11:02:02 +0300

    Curve window detection

--- ImageProcessing/OpeningPlacement.py
+++ ImageProcessing/OpeningPlacement.py
@@ -10,7 +10,7 @@ from Entities.Point import Point
 class OpeningPlacement(Stage):
     _name = 'opening_placement'
 
-    WIDER = -8
+    WIDER = -11
 
     def __init__(self):
         Stage().__init__()
@@ -22,7 +22,7 @@ class OpeningPlacement(Stage):
         for i, room in enumerate(self.img):
             new_room = copy.deepcopy(room)
             for opening in self.desc.openings:
-                new_item = self.find_opening_place(new_room.walls, opening)
+                new_item = self.find_opening_place(new_room, opening)
                 if new_item:
                     new_room.openings.append(new_item)
             res.append(new_room)
@@ -37,17 +37,21 @@ class OpeningPlacement(Stage):
         self.desc = res
         self.update_status(Stage.STATUS_SUCCEEDED)
 
-    def get_opening_rect(self, opening):
+    def get_opening_rect(self, opening, window_shift=-3, door_shift=None):
         new_item = copy.deepcopy(opening)
 
         p1 = opening.placement[0].point_1
         p2 = opening.placement[0].point_2
 
         new_item.placement = []
-
-        shift = -2
+        if not window_shift:
+            shift = -((self.parent.width + self.parent.height) / 2) / ((abs(p1.x - p2.x) + abs(p1.y - p2.y)) / 2) / 10
+        else:
+            shift = window_shift
         if opening._type != 'window':
             shift = OpeningPlacement.WIDER
+        if door_shift:
+            shift = door_shift
 
         _sx = int(min(p1.x, p2.x)) - shift
         _ex = int(max(p1.x, p2.x)) + shift
@@ -57,16 +61,68 @@ class OpeningPlacement(Stage):
         rect = Line(Point(_sx, _sy), Point(_ex, _ey))
         return new_item, rect
 
-    def find_opening_place(self, walls, opening):
-        new_item, rect = self.get_opening_rect(opening)
+    def is_curve_window(self, placement, walls):
+        line_in_room = []
+        count_big_angle = 0
+        lenght = 0
+        for line in placement:
+            for wall in walls:
+                if wall.inner_part.distance_to_point(line.point_1) == 0:
+                    if line_in_room:
+                        angle = line.angle_between(line_in_room[-1])
+                        if angle > 15 and angle < 75 or angle > 115 and angle < 165:
+                            count_big_angle += 1
+                    line_in_room.append(line)
+                    lenght += line.line_length()
 
-        if opening._type == 'item' or opening._type == 'arch':
-            new_item.placement.append(rect)
-        else:
+        if line_in_room:
+            distance = Line(line_in_room[0].point_1, line_in_room[-1].point_2)
+            if distance.line_length() != 0 and lenght / distance.line_length() > 2 and count_big_angle > 2:
+                return True
+
+        return False
+
+    def is_curve_door(self, placement, walls):
+        line_in_room = []
+        lenght = 0
+        for line in placement:
+            for wall in walls:
+                if wall.inner_part.distance_to_point(line.point_1) == 0:
+                    line_in_room.append(line)
+                    lenght += line.line_length()
+
+        if line_in_room:
+            distance = Line(line_in_room[0].point_1, line_in_room[-1].point_2)
+            if distance.line_length() != 0 and lenght / distance.line_length() > 1.1:
+                return True
+
+        return False
+
+
+    def find_opening_place(self, room, opening):
+        walls = room.walls
+
+        def find_intersections():
+            new_item.placement = []
             for wall in walls:
                 intersection = wall.inner_part.rectangle_intersection(rect)
                 if intersection:
                     new_item.placement.append(intersection)
                     continue
+
+        new_item, rect = self.get_opening_rect(opening)
+        if opening._type == 'item' or opening._type == 'arch':
+            new_item.placement.append(rect)
+            return new_item
+        else:
+            find_intersections()
+
+        if opening._type == 'window' and new_item.placement and self.is_curve_window(new_item.placement, walls):
+            new_item, rect = self.get_opening_rect(opening, window_shift=-25)
+            find_intersections()
+
+        if opening._type == 'door' and new_item.placement and self.is_curve_door(new_item.placement, walls):
+            new_item, rect = self.get_opening_rect(opening, door_shift=-15)
+            find_intersections()
 
         return new_item
