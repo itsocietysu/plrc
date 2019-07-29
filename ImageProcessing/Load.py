1f76cff490a756da716881a5adb80c2b181d7823
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-07-29 22:52:37 +0300

    Changed verbose
    - Created one window with two trackbars

--- ImageProcessing/Load.py
+++ ImageProcessing/Load.py
@@ -12,6 +12,13 @@ class Load(Stage):
 
     def process(self, parent):
         """load the data"""
+
+        def on_alpha_change(val):
+            parent.on_alpha_change(val)
+
+        def on_stage_change(val):
+            parent.on_stage_change(val)
+
         self.update_status(Stage.STATUS_RUNNING)
 
         self.img = cv2.imread(self.img)
@@ -25,6 +32,12 @@ class Load(Stage):
             for line in f:
                 parent.parameters_file.append(line)
             f.close()
+
+        if parent.verbose:
+            cv2.namedWindow(parent.window_name, cv2.WINDOW_NORMAL)
+            cv2.resizeWindow(parent.window_name, 1000, 800)
+            cv2.createTrackbar(parent.tr_alpha, parent.window_name, parent.max_alpha, parent.max_alpha, on_alpha_change)
+            cv2.createTrackbar(parent.tr_stage, parent.window_name, 0, parent.cnt_stages - 2, on_stage_change)
 
         self.update_status(Stage.STATUS_SUCCEEDED)
 
--- ImageProcessing/Pipeline.py
+++ ImageProcessing/Pipeline.py
@@ -1,6 +1,52 @@
 import cv2
+import numpy as np
+
 from ImageProcessing.Stage import Stage
 from Entities.Plan import Plan
+
+window_name = 'process'
+tr_alpha = 'alpha'
+tr_stage = 'stage'
+max_alpha = 100
+
+
+def show_image(img):
+    if img is None:
+        return
+    height, width, channels = img.shape
+    rect = cv2.getWindowImageRect(window_name)
+    h = rect[3]
+    w = int(h * width / height)
+    if w > rect[2]:
+        w = rect[2]
+        h = int(height / width * w)
+    new_img = cv2.resize(img, (w, h), interpolation=cv2.INTER_AREA)
+    s = np.zeros((rect[3], rect[2], 3), np.uint8)
+    s.fill(255)
+    sy = int((rect[3] - h) / 2)
+    sx = int((rect[2] - w) / 2)
+    s[sy:sy + h, sx:sx + w] = new_img
+    cv2.imshow(window_name, s)
+
+
+def on_track_change(images, alpha, stage):
+    if len(images) < stage or len(images) == 0 or images[stage] is None:
+        return
+
+    alpha = alpha / max_alpha
+    beta = (1.0 - alpha)
+    dst = cv2.addWeighted(images[stage], alpha, images[0], beta, 0.0)
+    show_image(dst)
+
+
+def on_alpha_change(images, val):
+    stage = cv2.getTrackbarPos(tr_stage, window_name)
+    on_track_change(images, val, stage)
+
+
+def on_stage_change(images, val):
+    alpha = cv2.getTrackbarPos(tr_alpha, window_name)
+    on_track_change(images, alpha, val)
 
 
 class Pipeline:
@@ -19,8 +65,21 @@ class Pipeline:
         self.plan = Plan()
         self.dxf = _save_dxf
 
+        self.cnt_stages = len(_pipeline)
+        self.images = []
+        self.window_name = window_name
+        self.tr_alpha = tr_alpha
+        self.tr_stage = tr_stage
+        self.max_alpha = max_alpha
+
+    def on_alpha_change(self, val):
+        on_alpha_change(self.images, val)
+
+    def on_stage_change(self, val):
+        on_stage_change(self.images, val)
+
     def process(self):
-        for stage in self.pipeline:
+        for i, stage in enumerate(self.pipeline):
             _ = stage()
             _.pass_data(self.img, self.desc, self.parameters_file, self.graph, self.label_map, self.dxf)
             _.process(self)
@@ -29,7 +88,10 @@ class Pipeline:
                 self.img, self.desc = _.retrieve_data()
 
                 if self.verbose:
-                    _.visualize_stage()
+                    self.images.append(_.visualize_stage())
+                    show_image(self.images[i])
+                    cv2.setTrackbarPos(tr_stage, window_name, i)
+                    cv2.waitKey(7)
 
             if _.status == Stage.STATUS_FAILED:
                 raise Exception('Stage %s, failed' % _._name)
