09248e6108647778ba094a764c9cbf4d1ef8adae
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-08-07 17:40:08 +0300

    Made small changes in Contours

--- ImageProcessing/Contours.py
+++ ImageProcessing/Contours.py
@@ -4,18 +4,19 @@ import numpy as np
 from ImageProcessing.Stage import Stage
 
 
-"""Binarize image and remove basical noize"""
 class Contours(Stage):
+    """Find contours of rooms"""
     _name = 'contours'
 
     def __init__(self):
         Stage().__init__()
-        self.w = 0
-        self.h = 0
+        self.shape = ()
 
     def process(self, parent):
-        """smooth the data"""
-        self.w, self.h = parent.width, parent.height
+        self.update_status(Stage.STATUS_RUNNING)
+
+        self.shape = (parent.height, parent.width, 3)
+
         res = []
         for img in self.img:
             gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -27,7 +28,6 @@ class Contours(Stage):
         self.update_status(Stage.STATUS_SUCCEEDED)
 
     def visualize_stage(self):
-        img = np.zeros((self.h, self.w, 3), np.uint8)
+        img = np.zeros(self.shape, np.uint8)
         cv2.drawContours(img, self.img, -1, (0, 0, 255), 3)
         return img
-
