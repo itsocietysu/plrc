4b3dcca645886fc9214bf543a4d9c4eeca2ee243
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-08-09 16:02:43 +0300

    Changed `run.py`

--- Run/run.py
+++ Run/run.py
@@ -1,19 +1,15 @@
-
-from ImageProcessing.NewLoad import Load
-from ImageProcessing.Preprocessing import Preprocessing
 from ImageProcessing.Components import Components
 from ImageProcessing.Contours import Contours
-from ImageProcessing.RoomConstructor import RoomConstructor
-from ImageProcessing.OpeningPlacement import OpeningPlacement
+from ImageProcessing.Load import Load
 from ImageProcessing.ObjectDetection import ObjectDetection
-from ImageProcessing.NewSave import Save
-
-
+from ImageProcessing.OpeningPlacement import OpeningPlacement
 from ImageProcessing.Pipeline import Pipeline
+from ImageProcessing.Preprocessing import Preprocessing
+from ImageProcessing.RoomConstructor import RoomConstructor
 
 
 def run(image, graph, label_map):
-    pipeline_steps = [Load, ObjectDetection, Preprocessing, Components, Contours, RoomConstructor, OpeningPlacement, Save]
+    pipeline_steps = [Load, ObjectDetection, Preprocessing, Components, Contours, RoomConstructor, OpeningPlacement]
 
     pipeline = Pipeline(pipeline_steps, _img=image, _graph=graph, _label_map=label_map, _verbose=False)
     pipeline.process()
